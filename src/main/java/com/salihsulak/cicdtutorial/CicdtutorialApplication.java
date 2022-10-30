952d6577064786851b9e5afe09b4f7ad1ea17233
Salih Sulak <salihsulak00@gmail.com>
2022-10-30 20:31:55 +0300

    initial commit

---
+++ src/main/java/com/salihsulak/cicdtutorial/CicdtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.salihsulak.cicdtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdtutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salihsulak/cicdtutorial/CicdtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salihsulak.cicdtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
